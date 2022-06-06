5ba50acaad4ced92c248b7f4711ccee19aeda94d
Ali Asad <aliasad78930@gmail.com>
2022-06-06 15:42:23 +0500

    PrintRecepit

--- home/src/components/employee/PrintReceipt.jsx
+++ home/src/components/employee/PrintReceipt.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-
+import "./PrintReceipt.css"
 const PrintReceipt = () => {
+  
+  function printReceipt(e) {
+    // document.getElementById("prnt-btn").style.display = "none";
+    window.print();
+    // document.getElementById("prnt-btn").style.display = "block";
+    // window.close();
+  }
   return (
-    <>
+    <div>
       <div className="print_main-page">
         <div className="print_header">
           <div className="print_shop-title">
@@ -13,7 +20,7 @@ const PrintReceipt = () => {
             </p>
           </div>
           <div className="print_order-id">
-            <h1>Order Id # </h1>
+            <h1>Order Id # 9999</h1>
           </div>
         </div>
       </div>
@@ -21,28 +28,28 @@ const PrintReceipt = () => {
         <div className="print_info-data">
           <table>
             <tr>
-              <th>Customer-Id: </th>
-              <td></td>
+              <th>Customer-Id:</th>
+              <td>9999</td>
             </tr>
             <tr>
               <th>Sold by: </th>
-              <td></td>
+              <td>Ali Asad</td>
             </tr>
             <tr>
               <th>Date Time: </th>
-              <td></td>
+              <td>06-06-2022 13:01</td>
             </tr>
             <tr>
               <th>Status: </th>
 
-              <td className="print_status print_paid"></td>
+              <td className="print_status print_paid">Paid</td>
 
-              <td className="print_status print_unpaid"></td>
+              {/* <td className="print_status print_unpaid"></td> */}
             </tr>
           </table>
         </div>
         <div className="print_prnt-btn-div">
-          <button id="prnt-btn">
+          <button onClick={printReceipt}>
             <i className="print_fas print_fa-print"></i> Print Receipt
           </button>{" "}
         </div>
@@ -60,24 +67,24 @@ const PrintReceipt = () => {
           </thead>
           <tbody>
             <tr>
-              <td className="print_t-left print_w-70"></td>
-              <td> Rs.</td>
-              <td></td>
-              <td> Rs.</td>
+              <td className="print_t-left print_w-70">Water 500ML</td>
+              <td> Rs. 999999</td>
+              <td>9999</td>
+              <td> Rs. 999999</td>
             </tr>
 
             <table>
               <tr>
                 <th className="print_w-70 print_t-right">Sub Total:</th>
-                <td className="print_w-30"> Rs.</td>
+                <td className="print_w-30"> Rs. 999999</td>
               </tr>
               <tr>
                 <th className="print_w-70 print_t-right">Discount:</th>
-                <td> Rs.</td>
+                <td> Rs. 000000</td>
               </tr>
               <tr>
                 <th className="print_w-70 print_t-right">Total:</th>
-                <td> Rupees</td>
+                <td> Rupees 999999</td>
               </tr>
             </table>
           </tbody>
@@ -86,7 +93,8 @@ const PrintReceipt = () => {
       <div className="print_thankyou-message">
         <h3>Shukriya!</h3>
       </div>
-    </>
+      <h1>Message</h1>
+    </div>
   );
 };
 export default PrintReceipt;
