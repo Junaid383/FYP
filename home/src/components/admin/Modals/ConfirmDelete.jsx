91e52cd5c5e0e5ba25605b7a7e4bf7705657a489
Ali Asad <aliasad78930@gmail.com>
2022-06-15 14:04:00 +0500

    MODAL done

--- home/src/components/admin/Modals/ConfirmDelete.jsx
+++ home/src/components/admin/Modals/ConfirmDelete.jsx
@@ -6,13 +6,20 @@ const ConfirmDelete = ({ close, onConfirm }) => {
   return ReactDOM.createPortal(
     <div className={classes.container} onClick={close}>
       <div className={classes.wrapper}>
+      <div className={classes.modaldialog}>
+      <div className={classes.modalcontent}>
       {/* <div className={classes.icon}>
 					<i class="material-icons">&#xE5CD;</i>
 				</div>	 */}
         <h4 class="modal-title w-100">Are you sure?</h4>	
-        <div>
+        <div className={classes.modalbody}>
+				<p>Do you really want to delete this records? This process cannot be undone.</p>
+			</div>
+        <div className={classes.modalfooter} >
           <button onClick={onConfirm} class="btn btn-danger">Confirm</button>
           <button onClick={close} class="btn btn-secondary">Cancel</button>
+        </div>
+        </div>
         </div>
       </div>
     </div>
